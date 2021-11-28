543b99ecbfb24a3fd877b908ea8fc1765ae35eb7
Marina8989 <marina.mill209@gmail.com>
2021-11-28 14:57:57 -0800

    react

--- src/App.js
+++ src/App.js
@@ -1,69 +1,65 @@
 import React from 'react';
 import './index.css';
 
-const Counter = (props) => {
-    return (
-      <>
-        {props.counters.map(item => {
-            const {id, counter} = item
-            return (
-            <div key={id}>
-              <h4>{counter}</h4>
-              <button onClick={() => props.handleClick(id)}>increment</button>
-            </div>
-          )
-        })}
-      </>
+
+const Item = (props) => {
+   return(
+       <li>{props.item.value}</li>
+   )
+}
+
+const List = (props) => {
+    return(
+       <ul>
+          {props.list.map(item => (
+             <div key={item.id}>
+                 <Item item={item}/>
+             </div>
+          ))}
+       </ul>
     )
+}
+
+class Form extends React.Component{
+    state={
+        searchInput: ''
+    }
+    handleChange = (e) => {
+       this.setState({searchInput: e.target.value})
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const value = this.state.searchInput;
+        this.setState({searchInput: ''});
+        this.props.handleSubmit(value);
+    }
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <input value={this.state.searchInput} onChange={this.handleChange}/>
+            </form>
+        )
+    }
 }
 
 class App extends React.Component{
     state={
-        counters: [
-            {id: 1, counter: 5, increment: 5},
-            {id: 2, counter: 10, increment: 3},
-            {id: 3, counter: 17, increment: 7},
-            {id: 4, counter: 20}
-        ],
-        isSorted: null
+        list: []
     }
-    handleClick = (id) => {
-       const newList = this.state.counters.map(item => {
-          if(item.id === id) {
-              item.counter += item.increment || 1;
-          }
-          return item
-       })
-       this.setState({counters: newList})
+    handleSubmit = (value) => {
+      const item = {
+          value,
+          id: Math.floor(Math.random() * 45)
+      }
+      const newList = [...this.state.list, item];
+      this.setState({list: newList})
     }
-    handleSorted = () => {
-        if(this.state.isSorted === null) {
-           this.setState({isSorted: true})
-        }
-        if(this.state.isSorted === true) {
-          this.setState({isSorted: false})
-        }
-        if(this.state.isSorted === false) {
-           this.setState({isSorted: null})
-        }
-    }
-    render() {
-        let sortedArr = [...this.state.counters]
-         if(this.state.isSorted){
-             sortedArr = sortedArr.sort((a, b) => {
-                 return a.counter - b.counter
-             })
-         }else if(this.state.isSorted === false) {
-            sortedArr = sortedArr.sort((a, b) => {
-                return b.counter - a.counter
-            })
-         }
+    render(){
         return(
            <div>
-               <h3>Counter App</h3>
-               <Counter handleClick={this.handleClick} counters={sortedArr}/>
-               <br />
-               <button onClick={this.handleSorted}>sort</button>
+               <Form handleSubmit={this.handleSubmit} />
+               <List list={this.state.list} />
            </div>
         )
     }
